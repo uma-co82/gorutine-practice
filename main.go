e3e71ecb65c90489d9df5227ff8a0a32fdbd0ff9
Yuta Isozaki <42496916+uma-co82@users.noreply.github.com>
2019-10-10 23:03:51 +0900

    Update main.go

--- main.go
+++ main.go
@@ -19,7 +19,9 @@ func main() {
 
 	// practice5()
 
-	practice6()
+	// practice6()
+
+	practice7()
 }
 
 /***************************************************************
@@ -168,6 +170,8 @@ func practice5() {
 }
 
 /***************************************************************
+ * ライブロック
+ * 廊下のすれ違いを避ける為両方のgoroutineが左右に移動している状態
  ***************************************************************/
 
 func practice6() {
@@ -219,4 +223,56 @@ func practice6() {
 	go walk(&peopleInHallway, "Alice")
 	go walk(&peopleInHallway, "Barbara")
 	peopleInHallway.Wait()
+}
+
+/***************************************************************
+ * リソース枯渇
+ * greedyWorkerはワークループ全体で共有ロックを保持している
+ * politeWorkerは必要な時だけロックしている
+ ***************************************************************/
+
+func practice7() {
+	var wg sync.WaitGroup
+	var sharedLock sync.Mutex
+	const runtime = 1 * time.Second
+
+	greedyWorker := func() {
+		defer wg.Done()
+
+		var count int
+		for begin := time.Now(); time.Since(begin) <= runtime; {
+			sharedLock.Lock()
+			time.Sleep(3 * time.Nanosecond)
+			sharedLock.Unlock()
+			count++
+		}
+		fmt.Printf("Greedy worker was able to execute %v work loops\n", count)
+	}
+
+	politeWorker := func() {
+		defer wg.Done()
+
+		var count int
+		for begin := time.Now(); time.Since(begin) <= runtime; {
+			sharedLock.Lock()
+			time.Sleep(1 * time.Nanosecond)
+			sharedLock.Unlock()
+
+			sharedLock.Lock()
+			time.Sleep(1 * time.Nanosecond)
+			sharedLock.Unlock()
+
+			sharedLock.Lock()
+			time.Sleep(1 * time.Nanosecond)
+			sharedLock.Unlock()
+
+			count++
+		}
+		fmt.Printf("Polite worker was able to execute %v work loops.\n", count)
+	}
+
+	wg.Add(2)
+	go greedyWorker()
+	go politeWorker()
+	wg.Wait()
 }
