79856682297a9d3016e6102e5cb9c6ab00512533
Yuta Isozaki <42496916+uma-co82@users.noreply.github.com>
2019-10-10 21:52:49 +0900

    Update main.go

--- main.go
+++ main.go
@@ -1,8 +1,10 @@
 package main
 
 import (
+	"bytes"
 	"fmt"
 	"sync"
+	"sync/atomic"
 	"time"
 )
 
@@ -15,7 +17,9 @@ func main() {
 
 	// practice4()
 
-	practice5()
+	// practice5()
+
+	practice6()
 }
 
 /***************************************************************
@@ -134,6 +138,13 @@ func practice4() {
 	memoryAccess.Unlock()
 }
 
+/***************************************************************
+ * Shared-memory :メモリを共有して通信をやり取りする
+ * デッドロック発生
+ * 各goroutineで a, b をロックしてその後 b, aをロックしようと無限に
+ * 待ち続けている状態 * ちなみに time.Slee()を外せばデッドロックは発生しない
+ ***************************************************************/
+
 func practice5() {
 	type value struct {
 		mu    sync.Mutex
@@ -154,4 +165,58 @@ func practice5() {
 	go printSum(&a, &b)
 	go printSum(&b, &a)
 	wg.Wait()
+}
+
+/***************************************************************
+ ***************************************************************/
+
+func practice6() {
+	cadence := sync.NewCond(&sync.Mutex{})
+	go func() {
+		for range time.Tick(1 * time.Millisecond) {
+			cadence.Broadcast()
+		}
+	}()
+
+	takeStep := func() {
+		cadence.L.Lock()
+		cadence.Wait()
+		cadence.L.Unlock()
+	}
+
+	tryDir := func(dirName string, dir *int32, out *bytes.Buffer) bool {
+		fmt.Fprintf(out, " %v", dirName)
+		atomic.AddInt32(dir, 1)
+		takeStep()
+		if atomic.LoadInt32(dir) == 1 {
+			fmt.Fprintf(out, ". Success!")
+			return true
+		}
+		takeStep()
+		atomic.AddInt32(dir, -1)
+		return false
+	}
+
+	var left, right int32
+	tryLeft := func(out *bytes.Buffer) bool { return tryDir("left", &left, out) }
+	tryRight := func(out *bytes.Buffer) bool { return tryDir("right", &right, out) }
+
+	walk := func(walking *sync.WaitGroup, name string) {
+		var out bytes.Buffer
+		defer func() { fmt.Println(out.String()) }()
+		defer walking.Done()
+		fmt.Fprintf(&out, "%v is trying to scoot:", name)
+		for i := 0; i < 5; i++ {
+			if tryLeft(&out) || tryRight(&out) {
+				return
+			}
+		}
+		fmt.Fprintf(&out, "\n%v tosses her hands up in exasperation!", name)
+	}
+
+	var peopleInHallway sync.WaitGroup
+	peopleInHallway.Add(2)
+	go walk(&peopleInHallway, "Alice")
+	go walk(&peopleInHallway, "Barbara")
+	peopleInHallway.Wait()
 }
