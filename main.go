1383ef5b57e7eb4a54562e7ce2887d17d8333549
Yuta Isozaki <42496916+uma-co82@users.noreply.github.com>
2019-11-16 18:16:14 +0900

    Update main.go

--- main.go
+++ main.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"math"
 	"math/rand"
+	"net/http"
 	"os"
 	"sync"
 	"sync/atomic"
@@ -88,7 +89,13 @@ func main() {
 
 	// practice37()
 
-	practice38()
+	// practice38()
+
+	// practice39()
+
+	// practice40()
+
+	practice41()
 }
 
 /***************************************************************
@@ -1224,5 +1231,40 @@ func practice40() {
  * エラーハンドリング
  ***************************************************************/
 
-func paractice41() {
+func practice41() {
+	type Result struct {
+		Error    error
+		Response *http.Response
+	}
+
+	checkStatus := func(done <-chan interface{}, urls ...string) <-chan Result {
+		results := make(chan Result)
+		go func() {
+			defer close(results)
+
+			for _, url := range urls {
+				var result Result
+				resp, err := http.Get(url)
+				result = Result{Error: err, Response: resp}
+				select {
+				case <-done:
+					return
+				case results <- result:
+				}
+			}
+		}()
+		return results
+	}
+
+	done := make(chan interface{})
+	defer close(done)
+
+	urls := []string{"https://www.google.com", "https://badhost"}
+	for result := range checkStatus(done, urls...) {
+		if result.Error != nil {
+			fmt.Printf("error: %v", result.Error)
+			continue
+		}
+		fmt.Printf("Response: %v\n", result.Response.Status)
+	}
 }
