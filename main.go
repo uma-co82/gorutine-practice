1aea2d41f56d43aaa943acdfba07a5a5cac2bb52
Yuta Isozaki <42496916+uma-co82@users.noreply.github.com>
2019-11-17 20:35:23 +0900

    Update main.go

--- main.go
+++ main.go
@@ -1319,6 +1319,8 @@ func practice42() {
 
 // practice42をチャネルを使って書き換える
 func practice43() {
+	// パイプラインの始めには常にチャネルへの変換を必要とするデータの塊がある
+	// ジェネレーターと呼ばれる
 	generator := func(done <-chan interface{}, integers ...int) <-chan int {
 		intStream := make(chan int, len(integers))
 		go func() {
@@ -1340,7 +1342,7 @@ func practice43() {
 			defer close(multipliedStream)
 			for i := range intStream {
 				select {
-				case <-done:
+				case <-done: // ゴルーチンリークを避ける
 					return
 				case multipliedStream <- i * multiplier:
 				}
@@ -1374,3 +1376,98 @@ func practice43() {
 		fmt.Println(v)
 	}
 }
+
+/***************************************************************
+ * 便利ジェネレーター
+ * パイプラインのジェネレーターは一塊の値をチャネル上のストリームに変換する関数
+ ***************************************************************/
+
+func practice44() {
+	repeat := func(done <-chan interface{}, values ...interface{}) <-chan interface{} {
+		valueStream := make(chan interface{})
+		go func() {
+			defer close(valueStream)
+			for {
+				for _, v := range values {
+					select {
+					case <-done:
+						return
+					case valueStream <- v:
+					}
+				}
+			}
+		}()
+		return valueStream
+	}
+
+	repeatFn := func(done <-chan interface{}, fn func() interface{}) <-chan interface{} {
+		valueStream := make(chan interface{})
+		go func() {
+			defer close(valueStream)
+			for {
+				select {
+				case <-done:
+					return
+				case valueStream <- fn():
+				}
+			}
+		}()
+		return valueStream
+	}
+
+	toString := func(done <-chan interface{}, valueStream <-chan interface{}) <-chan string {
+		stringStream := make(chan string)
+		go func() {
+			defer close(stringStream)
+			for v := range valueStream {
+				select {
+				case <-done:
+					return
+				case stringStream <- v.(string):
+				}
+			}
+		}()
+		return stringStream
+	}
+
+	take := func(done <-chan interface{}, valueStream <-chan interface{}, num int) <-chan interface{} {
+		takeStream := make(chan interface{})
+		go func() {
+			defer close(takeStream)
+			for i := 0; i < num; i++ {
+				select {
+				case <-done:
+					return
+				case takeStream <- <-valueStream:
+				}
+			}
+		}()
+		return takeStream
+	}
+
+	done := make(chan interface{})
+	defer close(done)
+
+	for num := range take(done, repeat(done, 1), 10) {
+		fmt.Printf("%v ", num)
+	}
+
+	rand := func() interface{} {
+		return rand.Int()
+	}
+
+	for num := range take(done, repeatFn(done, rand), 10) {
+		fmt.Println(num)
+	}
+
+	var message string
+	for token := range toString(done, take(done, repeat(done, "I", "am."), 5)) {
+		message += token
+	}
+
+	fmt.Printf("message: %s...", message)
+}
+
+/***************************************************************
+ * ファンアウト、ファンイン
+ ***************************************************************/
