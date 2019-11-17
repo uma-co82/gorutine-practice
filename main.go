0bd5daab7842bf0dd30a87d3e714e42975c7d3bc
Yuta Isozaki <42496916+uma-co82@users.noreply.github.com>
2019-11-17 17:51:24 +0900

    Update main.go

--- main.go
+++ main.go
@@ -1307,6 +1307,13 @@ func practice42() {
 }
 
 /***************************************************************
+ * ！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！
+ * ！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！
+ * !！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！重要！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！
+ * ！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！
+ * ！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！
+ ***************************************************************/
+/***************************************************************
  * パイプライン構築のベストプラクティス
  ***************************************************************/
 
@@ -1325,5 +1332,45 @@ func practice43() {
 			}
 		}()
 		return intStream
+	}
+
+	multiply := func(done <-chan interface{}, intStream <-chan int, multiplier int) <-chan int {
+		multipliedStream := make(chan int)
+		go func() {
+			defer close(multipliedStream)
+			for i := range intStream {
+				select {
+				case <-done:
+					return
+				case multipliedStream <- i * multiplier:
+				}
+			}
+		}()
+		return multipliedStream
+	}
+
+	add := func(done <-chan interface{}, intStream <-chan int, additive int) <-chan int {
+		addedStream := make(chan int)
+		go func() {
+			defer close(addedStream)
+			for i := range intStream {
+				select {
+				case <-done:
+					return
+				case addedStream <- i + additive:
+				}
+			}
+		}()
+		return addedStream
+	}
+
+	done := make(chan interface{})
+	defer close(done) // 各ゴルーチンをキルする
+
+	intStream := generator(done, 1, 2, 3, 4)
+	pipeline := multiply(done, add(done, multiply(done, intStream, 2), 1), 2)
+
+	for v := range pipeline {
+		fmt.Println(v)
 	}
 }
