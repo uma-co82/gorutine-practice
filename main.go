ae3faf5a48e1b8a632bc9b364ba9394eba21ed7d
Yuta Isozaki <42496916+uma-co82@users.noreply.github.com>
2019-11-24 19:55:08 +0900

    Update main.go

--- main.go
+++ main.go
@@ -1576,48 +1576,48 @@ func practice46() {
  ***************************************************************/
 
 func practice47() {
-	orDone := func(done, c <-chan interface{}) <-chan interface{} {
-		valStream := make(chan interface{})
-		go func() {
-			defer close(valStream)
-			for {
-				select {
-				case <-done:
-					return
-				case v, ok := <-c:
-					if ok == false {
-						return
-					}
-					select {
-					case valStream <- v:
-					case <-done:
-					}
-				}
-			}
-		}()
-		return valStream
-	}
+	// orDone := func(done, c <-chan interface{}) <-chan interface{} {
+	// 	valStream := make(chan interface{})
+	// 	go func() {
+	// 		defer close(valStream)
+	// 		for {
+	// 			select {
+	// 			case <-done:
+	// 				return
+	// 			case v, ok := <-c:
+	// 				if ok == false {
+	// 					return
+	// 				}
+	// 				select {
+	// 				case valStream <- v:
+	// 				case <-done:
+	// 				}
+	// 			}
+	// 		}
+	// 	}()
+	// 	return valStream
+	// }
 
-	tee := func(done <-chan interface{}, in <-chan interface{}) (_, _ <-chan interface{}) {
-		out1 := make(chan interface{})
-		out2 := make(chan interface{})
-		go func() {
-			defer close(out1)
-			defer close(out2)
-			for val := range orDone(done, in) {
-				var out1, out2 = out1, out2
-				for i := 0; i < 2; i++ {
-					select {
-					case out1 <- val:
-						out1 = nil // チャネルへの書き込みが終わったら、nilを代入してそれ以降の書き込みをブロックしてもう片方のチャネルに書き込みできるようにする
-					case out2 <- val:
-						out2 = nil
-					}
-				}
-			}
-		}()
-		return out1, out2
-	}
+	// tee := func(done <-chan interface{}, in <-chan interface{}) (_, _ <-chan interface{}) {
+	// 	out1 := make(chan interface{})
+	// 	out2 := make(chan interface{})
+	// 	go func() {
+	// 		defer close(out1)
+	// 		defer close(out2)
+	// 		for val := range orDone(done, in) {
+	// 			var out1, out2 = out1, out2
+	// 			for i := 0; i < 2; i++ {
+	// 				select {
+	// 				case out1 <- val:
+	// 					out1 = nil // チャネルへの書き込みが終わったら、nilを代入してそれ以降の書き込みをブロックしてもう片方のチャネルに書き込みできるようにする
+	// 				case out2 <- val:
+	// 					out2 = nil
+	// 				}
+	// 			}
+	// 		}
+	// 	}()
+	// 	return out1, out2
+	// }
 }
 
 /***************************************************************
